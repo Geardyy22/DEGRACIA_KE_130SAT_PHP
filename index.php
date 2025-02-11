6205b97c874160aacedbe3949564e3a923c11731
geardyy22 <degraciakellyjay@gmail.com>
2025-02-11 19:31:42 +0800

    Video_3_your_first_php_tag_lab

---
+++ index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Demo</title>
+</head>
+<body>
+    <h1>
+        <?php  
+            echo "Hello, World" 
+        ?>
+    </h1>
+    <p>
+        <?php 
+            echo "Emegesh, I just learned how to put strings in PHP!!"
+        ?>
+    </p>
+</body>
+</html>
